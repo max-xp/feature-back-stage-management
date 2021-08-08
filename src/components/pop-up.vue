fbbee0477213815049d0741cd8d3d9a3709df95f
M <yumingm72@gmail.com>
2021-08-08 17:57:56 +0800

    弹窗

---
+++ src/components/pop-up.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="tooltip-bak">
+    <div class="tooltip">
+      <div class="tooltip-box">
+        <div class="tooltip-header">
+          <span>{{ this.content.tit }}</span>
+          <span @click="close()">X</span>
+        </div>
+        <div class="tooltip-content">
+          <span>{{ this.content.text }}</span>
+
+        </div>
+        <div class="tooltip-foot">
+            <span class="" v-for="(item,idx) in this.content.button" :key="idx">
+                {{item.text}}
+            </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+        type:Object,
+        default(){
+            return ''
+        }
+        
+    },
+
+  },
+  methods:{
+      close(e){
+          console.log(e);
+          
+      }
+  }
+};
+</script>
+
+<style scoped>
+.tooltip-bak {
+  background: rgb(0, 0, 0, 0.1);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+.tooltip {
+  width: 400px;
+  height: 150px;
+  background: white;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.tooltip-box{
+    padding: 20px;
+}
+.tooltip-header {
+  display: flex;
+  justify-content: space-between;
+}
+.tooltip-header span{
+    display: block;
+}
+.tooltip-content{
+    margin: 20px 0 0 20px;
+}
+.tooltip-foot{
+    display: flex;
+    justify-content: flex-end;
+}
+.tooltip-foot span{
+    display: block;
+    margin-left: 20px;
+}
+</style>
--- src/view/default/index.vue
+++ src/view/default/index.vue
@@ -1,29 +1,47 @@
 <template>
     <div>
         <!-- <selector/> -->
-        <datas @type='updatatime'/>
+
+        <!-- <datas @type='updatatime'/> -->
+
+        <pop ref="flag" :content='this.content' :flag='flag' v-show="flag"/>
+        <button @click="asd()">弹窗</button>
     </div>
 </template>
 
 <script>
 // import selector from '../../components/selector.vue'
-import datas from '../../components/date.vue'
+
+// import datas from '../../components/date.vue'
+
+import pop from '../../components/pop-up.vue'
 
 export default{
     components:{
         // selector
-        datas
+        // datas
+        pop
     },
     methods:{
-        updatatime(data){
-            this.dataT.start=data.start
-            this.dataT.end=data.end
-            console.log(this.dataT);
+        // updatatime(data){
+        //     this.dataT.start=data.start
+        //     this.dataT.end=data.end
+        //     console.log(this.dataT);
+        // }
+        asd(){
+            this.$refs.flag.close(this.flag)
         }
     },
     data(){
         return{
-            dataT:{},
+            flag:false,
+            content:{
+                tit:'提示',
+                text:'此操作将永久删除该文件, 是否继续?',
+                button:[{text:'取消',type:1},{text:'确定',type:2}]
+            }
+            
+            // dataT:{},
         }
     }
 }
